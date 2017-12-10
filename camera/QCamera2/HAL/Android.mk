64e7db1d0343bdc2c3cf0d8bc946cb31ae8366a0
Srijith Bharadwaj <srijithdamerla2001@gmail.com>
2017-12-10 11:16:42 +0530

    QCamera2/HAL: update location for libgrallocs (#1)
    Fix for the compile time error: 
    device/zuk/ham/camera/QCamera2/HAL/QCamera2HWI.cpp:36:26: fatal error: gralloc_priv.h: No such file or directory
     #include <gralloc_priv.h>

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -37,8 +37,8 @@ endif
 LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../stack/common \
         frameworks/native/include/media/openmax \
-        hardware/qcom/display/libgralloc \
-        hardware/qcom/media/libstagefrighthw \
+        hardware/qcom/display/msm8974/libgralloc \
+        hardware/qcom/media/msm8974/libstagefrighthw \
         system/media/camera/include \
         $(LOCAL_PATH)/../../mm-image-codec/qexif \
         $(LOCAL_PATH)/../../mm-image-codec/qomx_core \
@@ -51,7 +51,7 @@ else
 LOCAL_CFLAGS += -DUSE_KK_CODE
 endif
 
-LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
+LOCAL_C_INCLUDES += hardware/qcom/display/msm8974/libgralloc
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
